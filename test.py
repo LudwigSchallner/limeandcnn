309748bcb69971e4ceecd03a3fb9f4350b7ac62f
Ludwig Schallner <tollpatsch93@gmail.com>
2018-06-12 11:14:29 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-img = cv2.imread("./test.jpg")
-img = cv2.resize(img, (299, 299))
-img = img.reshape(3, 299,299)
-plt.figure()
-plt.imshow(img)
-plt.show()
