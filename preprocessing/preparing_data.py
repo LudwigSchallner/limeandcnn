e6d6ad9f7ee74a07ff20472f2d80d7fe5157d9bb
Tollpatsch93 <tollpatsch93@gmail.com>
2018-11-09 18:36:29 +0100

    .

--- preprocessing/preparing_data.py
+++ preprocessing/preparing_data.py
@@ -7,7 +7,8 @@ import pickle
 import cv2 
 
 
-DATADIR = os.path.join('.','data', sys.argv[1],'train')
+DATADIR_TRAIN = os.path.join('.','data', sys.argv[1],'train')
+DATADIR_TEST = os.path.join('.','data', sys.argv[1],'test')
 if sys.argv[1] == "catdog":
     CATEGORIES = ["Cat","Dog"]
     IMG_SIZE = 100
@@ -15,11 +16,12 @@ else:
     raise NameError("Dataset "+sys.argv[1]+" not supported!")
 
 training_data = []
+test_data = []
 
 def create_training_data():
     for category in CATEGORIES:  
 
-        path = os.path.join(DATADIR,category) 
+        path = os.path.join(DATADIR_TRAIN,category) 
         print(path) 
         class_num = CATEGORIES.index(category)
 
@@ -33,8 +35,23 @@ def create_training_data():
             except Exception as e:
                 print("general exception", e, os.path.join(path,img))
 
+def create_test_data():
+    for category in CATEGORIES:  
+
+        path = os.path.join(DATADIR_TEST,category) 
+        print(path) 
+        class_num = CATEGORIES.index(category)
+
+        for img in tqdm(os.listdir(path)): 
+            try:
+                img_array = cv2.imread(os.path.join(path,img))# ,cv2.IMREAD_GRAYSCALE)  
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+
 create_training_data()
+create_test_data()
+
 print(len(training_data))
+print(len(test_data))
 
 random.shuffle(training_data)
 
@@ -48,11 +65,11 @@ for features, label in training_data:
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 3)#1)
 
 #X
-pickle_out = open(os.path.join('.',"data","X_"+sys.argv[1]+".pickle"),"wb")
+pickle_out = open(os.path.join('.',"data",sys.argv[1],"X_train_"+sys.argv[1]+".pickle"),"wb")
 pickle.dump(X, pickle_out)
 pickle_out.close()
 #y
-pickle_out = open(os.path.join('.',"data","y_"+sys.argv[1]+".pickle"),"wb")
+pickle_out = open(os.path.join('.',"data","y_train_"+sys.argv[1]+".pickle"),"wb")
 pickle.dump(y, pickle_out)
 pickle_out.close()
 
