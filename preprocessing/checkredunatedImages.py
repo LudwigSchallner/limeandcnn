37a831bd5a57c6693b3ade6e2a29a065cdab6200
Tollpatsch93 <tollpatsch93@gmail.com>
2018-11-29 21:02:17 +0100

    Check for Redunated Generated Images

--- preprocessing/checkredunatedImages.py
+++ preprocessing/checkredunatedImages.py
@@ -4,14 +4,35 @@ import glob
 import numpy
 import time
 import datetime
+import hashlib
 
+
+def hash_file(filename):
+   # use sha1 or sha256 or other hashing algorithm
+   h = hashlib.sha1()
+
+   # open file and read it in chunked
+   with open(filename,'rb') as file:
+       chunk = 0
+       while chunk != b'':
+           chunk = file.read(1024)
+           h.update(chunk)
+
+   # return string
+   return h.hexdigest()
 
 start_time = time.time()
-print(datetime.datetime.now().time())
-
 img_dir = sys.argv[1] 
 data_path = os.path.join(img_dir,'*g')
 files = glob.glob(data_path)
+
+list_files = list()
+for f1 in files:
+    list_files.append(hash_file(f1))
+print(len(list_files))
+list_files = set(list_files)
+print(len(list_files))
+""""
 something_went_wrong = False
 
 for f1 in files:
@@ -24,4 +45,5 @@ for f1 in files:
                 print("---")
 
 print(something_went_wrong)
+"""
 print("--- %s seconds ---" % (time.time() - start_time))
